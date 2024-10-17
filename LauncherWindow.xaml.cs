1248f2a1d973867c264e7fe178db185410682d11
notcoolg <mxavniiv@gmail.com>
2024-10-16 20:29:35 -0400

    0.9.0-EA pre2

---
+++ LauncherWindow.xaml.cs
@@ -0,0 +1,31 @@
+using System.Windows;
+
+namespace NovawerksApp
+{
+    public partial class LauncherWindow : Window
+    {
+        public LauncherWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for launching the selected addon
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AddonListBox.SelectedItem != null)
+            {
+                string selectedAddon = AddonListBox.SelectedItem.ToString();
+                MessageBox.Show($"Launching {selectedAddon}", "Addon Launcher", MessageBoxButton.OK, MessageBoxImage.Information);
+                
+                // Insert logic here to launch the selected addon
+
+                // For demonstration, we'll just close the launcher window after launching
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please select an addon to launch.", "No Addon Selected", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
